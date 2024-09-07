33c08484ff6433f5cfe5ad54c895cc6f5ca7b208
divyabajaj0025 <divyabajaj0025@gmail.com>
2024-09-07 16:54:18 -0500

    added type for deck of cards

--- blackjack.hs
+++ blackjack.hs
@@ -1,13 +1,20 @@
+import qualified Data.Set as Set
 data CardNum = CardI Int | Jack | Queen | King | Ace
     deriving (Eq, Show)
 
 data Suite = Diamonds | Spades | Hearts | Clubs
     deriving (Eq, Show)
 
+-- Card is a pair of suite and value
 type Card = (Suite, CardNum)
+-- Deck is a Set of cards
+type Deck = Set Card
 
 cardValue :: CardNum -> Int
 cardValue (CardI n) = n
 cardValue Jack = 11
 cardValue Queen = 12
 cardValue King = 13
+cardValue Ace = 1
+
+
